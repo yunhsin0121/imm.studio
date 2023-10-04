2a3e3f2f005a3ceb45527968be4538a280d68873
yunhsin <alice334830@gmail.com>
2023-10-04 22:53:51 +0800

    update

--- javascript/js.js
+++ javascript/js.js
@@ -29,14 +29,37 @@ $(document).ready(function () {
             $('.btn-up').fadeOut();
         }
     });
-     $('.circle-link').hover(function () {
+    $('.circle-link').hover(function () {
             // over
             $('.circle-item').addClass('is-active');
         }, function () {
             // out
             $('.circle-item').removeClass('is-active');
         }
-     );
+    );
+
+    // Q&A選單
+    $('.accordionEyelash').click(function () {
+            // over
+            $('#accordionEyelash').removeClass('d-none');
+            $('#accordionEyelash').addClass('d-block');
+            $('#accordionEyelash').siblings().removeClass('d-block');
+            $('#accordionEyelash').siblings().addClass('d-none');
+    });
+    $('.accordionEyebrow').click(function () {
+        // over
+        $('#accordionEyebrow').removeClass('d-none');
+        $('#accordionEyebrow').addClass('d-block');
+        $('#accordionEyebrow').siblings().removeClass('d-block');
+        $('#accordionEyebrow').siblings().addClass('d-none');
+    });
+    $('.accordionColorRemoval').click(function () {
+        // over
+        $('#accordionColorRemoval').removeClass('d-none');
+        $('#accordionColorRemoval').addClass('d-block');
+        $('#accordionColorRemoval').siblings().removeClass('d-block');
+        $('#accordionColorRemoval').siblings().addClass('d-none');
+    });
 });
 
 AOS.init();
@@ -71,3 +94,17 @@ var indexSwiper = new Swiper(".indexSwiper", {
     effect: "cards",
     grabCursor: true,
   });
+
+let navTabs = document.querySelector('.nav-tabs')
+let navTabsItem = document.querySelectorAll('.nav-tabs li');
+// console.log(navTabs);
+// console.log(navTabsItem);
+navTabs.addEventListener('click',e=>{
+    console.log(e.target);
+    navTabsItem.forEach(item=>{
+        item.children[0].classList.remove('active');
+    })
+    // console.log(navTabs);
+    e.target.classList.add('active');
+    // console.log(navTabs);
+})
